7ec0388d10d656d2b197f89c4737a2562388e60a
wwx <gstswwx@outlook.com>
2016-08-14 19:45:32 +0800

    Singleton in java

---
+++ Singleton.java
@@ -0,0 +1,9 @@
+public class Singleton {
+	private static Singleton s = null;
+	private Singleton() {}
+	public static synchronized Singleton getInstance() {
+		if(s == null)
+			s = new Singleton();
+		return s;
+	}
+}
